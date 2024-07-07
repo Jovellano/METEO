d8f3a727011dea30d2ce5f0426fcdfb44bed316e
Jovellano <113550172+Jovellano@users.noreply.github.com>
2024-07-07 19:32:33 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+function seacrhCity(city) {
+  let apiKey = "3dfbt24a697c355eb92bof5b6004aa5c";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&unidad=metric";
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   //console.log(searchInput.value);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.ariaValue;
+  seacrhCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("seacrh-form");
 
