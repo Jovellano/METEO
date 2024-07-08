4a2884dac379e7dd8582cde2c665253305733e9a
Jovellano <113550172+Jovellano@users.noreply.github.com>
2024-07-07 21:20:10 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function refreshclima(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
 
-  console.log(response.data.descripción);
+  console.log(response.data.description);
 
   temperatureElement.innerHTML = Math.round(temperature);
   //console.log(response.data.temperature.current);
